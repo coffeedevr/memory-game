d379ac39569a394e733d61f73d5fec17d658eee9
coffeedevr <122944372+coffeedevr@users.noreply.github.com>
2023-04-15 00:02:19 +0800

    Add a main menu that initiates the game

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ const App = () => {
   const [bestScore, setBestScore] = useState(0)
   const [cardTrack, setCardTrack] = useState([])
   const [cards, setCards] = useState([])
+  const [game, setGame] = useState(false)
+  // const [over, setOver] = useState(false)
 
   // fetch poke-data during mount stage
   useEffect(() => {
@@ -46,7 +48,7 @@ const App = () => {
   }, [])
 
   // empty callback to re-render when score and cardTrack updates
-  useEffect(() => {}, [score, cardTrack, bestScore])
+  useEffect(() => {}, [score, cardTrack, bestScore, game])
 
   const selectCard = (event) => {
     const check = event.target.id
@@ -64,13 +66,41 @@ const App = () => {
     }
   }
 
+  const LoadGame = (props) => {
+    console.log(props.game)
+    return props.game ? <LoadCard cards={cards}/> : <Menu />
+  }
+
+  const Menu = () => {
+    return <div className="menu">
+      <h1 id='instructions-header'>Instructions:</h1>
+      <p id='instructions'>
+        There will be 10 random Pokemon that will display on your screen.<br/>
+        The goal is to pick ALL Pokemons without picking them twice.<br/>
+        Every time you click a Pokemon, their positions will be shuffled.
+      </p>
+      <button id="start-game" type="button" onClick={()=>{setGame(true)}}>START GAME</button>
+      </div>
+  }
+
+  const LoadCard = (props) => {
+   return (
+    <div className="LoadGame">
+      {Lodash.shuffle(props.cards).map(pokemon =>
+        <div className='pokecards' key={pokemon[0]}><img src={pokemon[2]} id={pokemon[0]} onClick={selectCard} alt={"picture of " + pokemon[1]}/>{pokemon[1]}</div>
+      )}
+    </div>
+    )
+  }
+
   return (
     <>
       <Header score={score} bestScore={bestScore}/>
       <div className="App">
-        {Lodash.shuffle(cards).map(pokemon =>
+      {/* {Lodash.shuffle(cards).map(pokemon =>
           <div className='pokecards' key={pokemon[0]}><img src={pokemon[2]} id={pokemon[0]} onClick={selectCard} alt={"picture of " + pokemon[1]}/>{pokemon[1]}</div>
-        )}
+        )} */}
+        <LoadGame game={game}/>
       </div>
       <Footer />
     </>
