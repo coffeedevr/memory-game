e4d3a9f7959115677355f7dd3a0428693af96505
coffeedevr <122944372+coffeedevr@users.noreply.github.com>
2023-04-14 14:12:01 +0800

    Added footer logo and changed html logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from "react"
+import GithubLogo from '../assets/githublogo.png'
 
 function Footer () {
     const footerText = '© ' + new Date().getFullYear() + ' coffeedevr | '
 
     return <div className="footer-component">
-        <p id="footer-text">{footerText}</p>
+        <p id="footer-text">{footerText}</p><a href=""><img id="footer-logo" src={GithubLogo} alt="github logo"/></a>
     </div>
 }
 
