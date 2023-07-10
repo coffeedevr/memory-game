e9cb8128f9da0dc72322d49344b143308ac21ab1
coffeedevr <122944372+coffeedevr@users.noreply.github.com>
2023-07-10 15:21:12 +0800

    Cleaned up code by removing and changing the ff:
    1. Removed declared components inside App component and moved them outside
    2. Destructured props in child components
    3. API fetch now happens when the start game is enabled inside of on component mount.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,78 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Lodash from "lodash"
 
+
+const LoadGame = ({game, cards, over, round, selectCard, setCards,  setGame, setOver, fetchPoke, setRound}) => {
+
+  return (
+    game ? 
+    <LoadCard cards={cards} over={over} round={round} selectCard={selectCard} setRound={setRound} setCards={setCards} setGame={setGame}/> 
+    : <Menu setGame={setGame}  setOver={setOver} fetchPoke={fetchPoke}/>
+  )
+}
+
+const Menu = ({setGame, setOver, fetchPoke}) => {
+  return <div className="menu">
+    <h1 className='menu-header'>Instructions:</h1>
+    <p className='menu-text'>
+      There will be 10 random Pokemon that will display on your screen.<br/>
+      The goal is to pick ALL Pokemons without picking them twice.<br/>
+      Every time you click a Pokemon, their positions will be shuffled.
+    </p>
+    <button className="btn-game" type="button"
+    onClick={()=>{
+      setGame(true)
+      setOver(false)
+      fetchPoke()
+    }}>START GAME</button>
+    </div>
+}
+
+const LoadCard = ({over, round, cards, selectCard, setRound, setCards, setGame}) => {
+ if (over === false) {
+ return (
+  <>
+  <div className="round-container">
+    <h1 className="round-text">Round {round}</h1>
+  </div>
+  {
+    cards ? 
+      <div className="LoadGame">
+        {Lodash.shuffle(cards).map(pokemon =>
+          <div className='pokecards' key={pokemon[0]}><img src={pokemon[2]} id={pokemon[0]} onClick={selectCard} alt={"picture of " + pokemon[1]}/>{pokemon[1]}</div>
+        )}
+      </div> : <p>Loading...</p>
+  }
+  </>
+  )
+ } else {
+  return (
+    <div className="menu">
+      <h1 className="menu-header">Game Over!</h1>
+      <h2 className="menu-text">Wanna try again?</h2>
+      <button className="btn-game" type="button" onClick={()=>{
+        setGame(false)
+        setRound(1)
+        setCards(null)
+        }}>RESTART</button>
+    </div>
+  )
+ }
+}
+
 const App = () => {
   const [score, setScore] = useState(0)
   const [bestScore, setBestScore] = useState(0)
   const [cardTrack, setCardTrack] = useState([])
-  const [cards, setCards] = useState([])
+  const [cards, setCards] = useState(null)
   const [game, setGame] = useState(false)
   const [over, setOver] = useState(false)
   const [round, setRound] = useState(1)
 
+  useEffect(() => {}, [score, cardTrack, bestScore, game, over, round])
+
   // fetch poke-data during mount stage
-  useEffect(() => {
+  const fetchPoke = () => {
 
     let randomArray = [];
     while(randomArray.length <= 10) {
@@ -48,10 +109,7 @@ const App = () => {
       setCards(list)
     })();
   
-  }, [round])
-
-  // empty callback to re-render when score and cardTrack updates
-  useEffect(() => {}, [score, cardTrack, bestScore, game, over])
+  }
 
   const selectCard = (event) => {
     const check = event.target.id
@@ -68,6 +126,7 @@ const App = () => {
       }
       if  ((score + 1)  === ( 10 * round)) {
         setCardTrack([])
+        fetchPoke()
         setRound(round + 1)
       } else {
         setCardTrack(cardTrack.concat(check))
@@ -75,62 +134,20 @@ const App = () => {
     }
   }
 
-  const LoadGame = (props) => {
-    return props.game ? <LoadCard cards={cards} over={over} round={round}/> : <Menu />
-  }
-
-  const Menu = () => {
-    return <div className="menu">
-      <h1 className='menu-header'>Instructions:</h1>
-      <p className='menu-text'>
-        There will be 10 random Pokemon that will display on your screen.<br/>
-        The goal is to pick ALL Pokemons without picking them twice.<br/>
-        Every time you click a Pokemon, their positions will be shuffled.
-      </p>
-      <button className="btn-game" type="button"
-      onClick={()=>{
-        setGame(true)
-        setOver(false)
-      }}>START GAME</button>
-      </div>
-  }
-
-  const LoadCard = (props) => {
-   if (props.over === false) {
-   return (
-    <>
-    <div className="round-container">
-      <h1 className="round-text">Round {props.round}</h1>
-    </div>
-    <div className="LoadGame">
-      {Lodash.shuffle(props.cards).map(pokemon =>
-        <div className='pokecards' key={pokemon[0]}><img src={pokemon[2]} id={pokemon[0]} onClick={selectCard} alt={"picture of " + pokemon[1]}/>{pokemon[1]}</div>
-      )}
-    </div>
-    </>
-    )
-   } else {
-    return (
-      <div className="menu">
-        <h1 className="menu-header">Game Over!</h1>
-        <h2 className="menu-text">Wanna try again?</h2>
-        <button className="btn-game" type="button" onClick={()=>{
-          setGame(false)
-          }}>RESTART</button>
-      </div>
-    )
-   }
-  }
 
   return (
     <>
       <Header score={score} bestScore={bestScore}/>
       <div className="App">
-        <LoadGame game={game} over={over} />
+        {/* <LoadGame round={round} game={game} over={over} cards={cards} setGame={setGame} setOver={setOver} setCards={setCards} setRound={setRound}  selectCard={selectCard} fetchPoke={fetchPoke}/> */}
+        game ?
+        <LoadCard cards={cards} over={over} round={round} selectCard={selectCard} setRound={setRound} setCards={setCards} setGame={setGame}/> 
+        : <Menu setGame={setGame}  setOver={setOver} fetchPoke={fetchPoke}/>
       </div>
       <Footer />
     </>
   );
 }
+
 
 export default App;
